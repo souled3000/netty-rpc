c6d6cc9260a72bb5ab5d6b4d6deb5e108dbc8714
caojian <caojian@erhsh.com>
2014-06-10 17:59:24 +0800

    add common api response

---
+++ src/main/java/com/github/com/nettyrpc/powersocket/dao/pojo/ApiResponse.java
@@ -0,0 +1,50 @@
+package com.github.com.nettyrpc.powersocket.dao.pojo;
+
+/**
+ * 通用API请求响应类
+ * 
+ * @author j
+ * 
+ */
+public class ApiResponse {
+
+	/**
+	 * 执行结果，0为执行成功
+	 */
+	private int status;
+
+	/**
+	 * 执行结果的描述，只有部分status会有描述
+	 */
+	private String statusMsg;
+
+	/**
+	 * 请求API时的URL，只包括url中"/api"部分之后的内容
+	 */
+	private String urlOrigin;
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public String getStatusMsg() {
+		return statusMsg;
+	}
+
+	public void setStatusMsg(String statusMsg) {
+		this.statusMsg = statusMsg;
+	}
+
+	public String getUrlOrigin() {
+		return urlOrigin;
+	}
+
+	public void setUrlOrigin(String urlOrigin) {
+		this.urlOrigin = urlOrigin;
+	}
+
+}
