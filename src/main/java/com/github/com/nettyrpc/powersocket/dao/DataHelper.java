54640000b883efb716bfd2ff25941d5a64fd1c61
caojian <caojian@erhsh.com>
2014-06-10 17:57:31 +0800

    add DataHelper

--- src/main/java/com/github/com/nettyrpc/powersocket/dao/DataHelper.java
+++ src/main/java/com/github/com/nettyrpc/powersocket/dao/DataHelper.java
@@ -1,7 +1,30 @@
 package com.github.com.nettyrpc.powersocket.dao;
 
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 public class DataHelper {
-//	private static final JedisPool pool = JedisPool;
+	private static final JedisPool pool;
+
+	static {
+		JedisPoolConfig poolConfig = new JedisPoolConfig();
+		poolConfig.setMaxIdle(1000);
+		poolConfig.setMaxWaitMillis(1000000);
+
+		String host = "193.168.1.14";
+		int port = 6379;
+		int timeout = 1000;
+		String password = null;
+
+		pool = new JedisPool(poolConfig, host, port, timeout, password);
+	}
+
+	public static Jedis getJedis() {
+		return pool.getResource();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(DataHelper.getJedis());
+	}
 }
