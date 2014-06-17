6634e289d14710a5cd8c65cf18c10a3bb3b20211
steven <steven.cuixin@gmail.com>
2014-06-17 10:36:47 +0800

    refactory InternalException.

--- src/main/java/com/github/com/nettyrpc/exception/InternalException.java
+++ src/main/java/com/github/com/nettyrpc/exception/InternalException.java
@@ -9,7 +9,7 @@ public class InternalException extends Exception {
 
 	private static final long serialVersionUID = 6112878534324677315L;
 
-	public InternalException(String code) {
-		super("ErrorCode" + code);
+	public InternalException(String message) {
+		super(message);
 	}
 }
