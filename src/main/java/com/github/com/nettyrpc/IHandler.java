1810016c6b711b7101dd800dfe263ca8f8b11159
steven <steven.cuixin@gmail.com>
2014-06-17 10:37:41 +0800

    refactory IHandler

--- src/main/java/com/github/com/nettyrpc/IHandler.java
+++ src/main/java/com/github/com/nettyrpc/IHandler.java
@@ -1,5 +1,7 @@
 package com.github.com.nettyrpc;
 
+import com.github.com.nettyrpc.exception.InternalException;
+
 public interface IHandler {
-	public Object rpc(RpcRequest req) throws Exception;
+	public Object rpc(RpcRequest req) throws InternalException;
 }
