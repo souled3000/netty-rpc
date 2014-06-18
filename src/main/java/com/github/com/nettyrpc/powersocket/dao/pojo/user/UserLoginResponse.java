d22289c0ae1f59e359a7b2a3b3a73a6ae81677c6
caojian <caojian@erhsh.com>
2014-06-18 17:33:52 +0800

    add user login function.

---
+++ src/main/java/com/github/com/nettyrpc/powersocket/dao/pojo/user/UserLoginResponse.java
@@ -0,0 +1,46 @@
+package com.github.com.nettyrpc.powersocket.dao.pojo.user;
+
+import com.github.com.nettyrpc.powersocket.dao.pojo.ApiResponse;
+
+public class UserLoginResponse extends ApiResponse {
+	private String userId;
+
+	private String cookie;
+
+	private String wbKey;
+
+	private String websocketAddr;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getCookie() {
+		return cookie;
+	}
+
+	public void setCookie(String cookie) {
+		this.cookie = cookie;
+	}
+
+	public String getWbKey() {
+		return wbKey;
+	}
+
+	public void setWbKey(String wbKey) {
+		this.wbKey = wbKey;
+	}
+
+	public String getWebsocketAddr() {
+		return websocketAddr;
+	}
+
+	public void setWebsocketAddr(String websocketAddr) {
+		this.websocketAddr = websocketAddr;
+	}
+
+}
---
+++ src/main/java/com/github/com/nettyrpc/powersocket/handler/UserLoginHandler.java
@@ -0,0 +1,76 @@
+package com.github.com.nettyrpc.powersocket.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import redis.clients.jedis.Jedis;
+
+import com.github.com.nettyrpc.IHandler;
+import com.github.com.nettyrpc.RpcRequest;
+import com.github.com.nettyrpc.exception.InternalException;
+import com.github.com.nettyrpc.powersocket.dao.DataHelper;
+import com.github.com.nettyrpc.powersocket.dao.pojo.user.UserLoginResponse;
+import com.github.com.nettyrpc.util.CookieUtil;
+import com.github.com.nettyrpc.util.HttpUtil;
+import com.github.com.nettyrpc.util.PBKDF2;
+
+public class UserLoginHandler implements IHandler {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(UserLoginHandler.class);
+
+	@Override
+	public Object rpc(RpcRequest req) throws InternalException {
+		logger.info("request: {}", req);
+
+		UserLoginResponse result = new UserLoginResponse();
+		result.setStatus(-1);
+		result.setStatusMsg("");
+		result.setUrlOrigin(req.getUrlOrigin());
+
+		String email = HttpUtil.getPostValue(req.getParams(), "email");
+		String passwd = HttpUtil.getPostValue(req.getParams(), "passwd");
+
+		Jedis jedis = null;
+		try {
+			jedis = DataHelper.getJedis();
+
+			// 1. 根据Email获取userId
+			String userId = jedis.hget("user:mailtoid", email);
+			if (null == userId) {
+				result.setStatusMsg("User not exist.");
+				return result;
+			}
+
+			// 2. encodePwd与passwd加密后的串做比较
+			String encodePwd = jedis.hget("user:shadow", userId);
+			if (!PBKDF2.validate(passwd, encodePwd)) {
+				result.setStatusMsg("Password error.");
+				return result;
+			}
+
+			String cookie = CookieUtil.encode(userId, CookieUtil.EXPIRE_SEC);
+			String wbKey = CookieUtil.generateKey(userId,
+					String.valueOf(System.currentTimeMillis()),
+					CookieUtil.EXPIRE_SEC);
+			String websocketAddr = CookieUtil.getWebsocketAddr();
+
+			result.setStatus(0);
+			result.setUserId(userId);
+			result.setCookie(cookie);
+			result.setWbKey(wbKey);
+			result.setWebsocketAddr(websocketAddr);
+
+		} catch (Exception e) {
+			DataHelper.returnBrokenJedis(jedis);
+			logger.error("User login error.");
+			throw new InternalException(e.getMessage());
+		} finally {
+			DataHelper.returnJedis(jedis);
+		}
+
+		logger.info("response: {}", result);
+		return result;
+	}
+
+}
