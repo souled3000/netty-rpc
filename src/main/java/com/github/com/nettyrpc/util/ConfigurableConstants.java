f63790b53a9ef2c5d213aabea1354f0e66e9f935
caojian <caojian@erhsh.com>
2014-06-23 18:37:28 +0800

    add property file's reader

---
+++ src/main/java/com/github/com/nettyrpc/util/ConfigurableConstants.java
@@ -0,0 +1,44 @@
+package com.github.com.nettyrpc.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConfigurableConstants {
+
+	protected static final Logger logger = LoggerFactory
+			.getLogger(ConfigurableConstants.class);
+
+	protected static final Properties p = new Properties();
+
+	protected static void init(String propertyFileName) {
+		logger.info("init: {}", propertyFileName);
+		InputStream is = null;
+		try {
+			is = ConfigurableConstants.class.getClassLoader()
+					.getResourceAsStream(propertyFileName);
+
+			if (null != is) {
+				p.load(is);
+			}
+		} catch (IOException e) {
+			logger.error("load property file: {} error!", propertyFileName);
+		} finally {
+			if (null != is) {
+				try {
+					is.close();
+				} catch (IOException e) {
+					logger.error("close " + propertyFileName + " error");
+				}
+			}
+		}
+	}
+
+	protected static String getProperty(String key, String defaultValue) {
+		return p.getProperty(key, defaultValue);
+	}
+
+}
