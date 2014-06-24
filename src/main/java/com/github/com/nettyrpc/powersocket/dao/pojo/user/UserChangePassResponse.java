cf0d112d869fcbeeda4f9d7dad18c5e6401839d9
caojian <caojian@erhsh.com>
2014-06-24 16:12:53 +0800

    add user change password api

---
+++ src/main/java/com/github/com/nettyrpc/powersocket/dao/pojo/user/UserChangePassResponse.java
@@ -0,0 +1,7 @@
+package com.github.com.nettyrpc.powersocket.dao.pojo.user;
+
+import com.github.com.nettyrpc.powersocket.dao.pojo.ApiResponse;
+
+public class UserChangePassResponse extends ApiResponse{
+
+}
---
+++ src/main/java/com/github/com/nettyrpc/powersocket/handler/UserChangePassHandler.java
@@ -0,0 +1,69 @@
+package com.github.com.nettyrpc.powersocket.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import redis.clients.jedis.Jedis;
+
+import com.github.com.nettyrpc.IHandler;
+import com.github.com.nettyrpc.RpcRequest;
+import com.github.com.nettyrpc.exception.InternalException;
+import com.github.com.nettyrpc.powersocket.dao.DataHelper;
+import com.github.com.nettyrpc.powersocket.dao.pojo.user.UserChangePassResponse;
+import com.github.com.nettyrpc.util.HttpUtil;
+import com.github.com.nettyrpc.util.PBKDF2;
+
+public class UserChangePassHandler implements IHandler {
+	private static final Logger logger = LoggerFactory
+			.getLogger(UserChangePassHandler.class);
+
+	@Override
+	public Object rpc(RpcRequest req) throws InternalException {
+		logger.info("request: {}", req);
+
+		UserChangePassResponse result = new UserChangePassResponse();
+		result.setStatus(-1);
+		result.setStatusMsg("");
+		result.setUrlOrigin(req.getUrlOrigin());
+
+		String userId = HttpUtil.getPostValue(req.getParams(), "userId");
+		String oldPass = HttpUtil.getPostValue(req.getParams(), "oldPass");
+		String newPass = HttpUtil.getPostValue(req.getParams(), "newPass");
+
+		Jedis jedis = null;
+		try {
+			jedis = DataHelper.getJedis();
+
+			// 1. 用户密码
+			String shadow = jedis.hget("user:shadow", userId);
+			if (null == shadow) {
+				result.setStatusMsg("userId not exist.");
+				return result;
+			}
+
+			// 2. 校验密码是否正确
+			if (!PBKDF2.validate(oldPass, shadow)) {
+				result.setStatusMsg("Password is incorrect.");
+				return result;
+			}
+
+			// 3. 生成新密码
+			String newShadow = PBKDF2.encode(newPass);
+			jedis.hset("user:shadow", userId, newShadow);
+
+			result.setStatus(0);
+
+		} catch (Exception e) {
+			DataHelper.returnBrokenJedis(jedis);
+			logger.error("User change password error");
+			throw new InternalException(e.getMessage());
+		} finally {
+			DataHelper.returnJedis(jedis);
+		}
+
+		logger.info("response: {}", result);
+		return result;
+
+	}
+
+}
