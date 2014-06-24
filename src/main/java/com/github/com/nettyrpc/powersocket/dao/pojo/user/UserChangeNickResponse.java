9930394ae29f62f2e984d01dc6c23865b1e731fb
caojian <caojian@erhsh.com>
2014-06-24 16:12:27 +0800

    add user change nick api

---
+++ src/main/java/com/github/com/nettyrpc/powersocket/dao/pojo/user/UserChangeNickResponse.java
@@ -0,0 +1,7 @@
+package com.github.com.nettyrpc.powersocket.dao.pojo.user;
+
+import com.github.com.nettyrpc.powersocket.dao.pojo.ApiResponse;
+
+public class UserChangeNickResponse extends ApiResponse {
+
+}
