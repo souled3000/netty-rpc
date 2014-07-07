438bda5bcb9330a09cb69ee04779671bf0d32868
licj <licj@blackcrystalinfo.com>
2014-07-07 11:01:12 +0800

    所有IHandler移入mapping.properties

--- src/main/java/com/github/com/nettyrpc/powersocket/Main.java
+++ src/main/java/com/github/com/nettyrpc/powersocket/Main.java
@@ -1,76 +1,31 @@
 package com.github.com.nettyrpc.powersocket;
 
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
+import java.io.IOException;
+import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.github.com.nettyrpc.HandlerManager;
-import com.github.com.nettyrpc.codec.RpcServerInitializer;
+import com.github.com.nettyrpc.IHandler;
 import com.github.com.nettyrpc.examples.App;
 import com.github.com.nettyrpc.exception.HandlerExistedException;
-import com.github.com.nettyrpc.powersocket.handler.BindInHandler;
-import com.github.com.nettyrpc.powersocket.handler.BindOutHandler;
-import com.github.com.nettyrpc.powersocket.handler.DeviceLoginHandler;
-import com.github.com.nettyrpc.powersocket.handler.DeviceRegisterHandler;
-import com.github.com.nettyrpc.powersocket.handler.GroupInfoHandler;
-import com.github.com.nettyrpc.powersocket.handler.GroupUploadHandler;
-import com.github.com.nettyrpc.powersocket.handler.UpgradeInfoHandler;
-import com.github.com.nettyrpc.powersocket.handler.UpgradeUploadHandler;
-import com.github.com.nettyrpc.powersocket.handler.UserChangeNickHandler;
-import com.github.com.nettyrpc.powersocket.handler.UserDevicesHandler;
-import com.github.com.nettyrpc.powersocket.handler.UserLoginHandler;
-import com.github.com.nettyrpc.powersocket.handler.UserChangePassHandler;
-import com.github.com.nettyrpc.powersocket.handler.UserRegisterHandler;
 
 public class Main {
 	private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-	private final int port;
 
-	public Main(int port) {
-		this.port = port;
-	}
-
-	public void run() throws Exception {
-		EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-		EventLoopGroup workerGroup = new NioEventLoopGroup(4);
-		try {
-			ServerBootstrap b = new ServerBootstrap();
-			b.option(ChannelOption.SO_BACKLOG, 1024);
-			b.group(bossGroup, workerGroup)
-					.channel(NioServerSocketChannel.class)
-					.childHandler(new RpcServerInitializer());
-
-			Channel ch = b.bind(port).sync().channel();
-			ch.closeFuture().sync();
-		} finally {
-			bossGroup.shutdownGracefully();
-			workerGroup.shutdownGracefully();
+	public static void bindHandler() throws HandlerExistedException, IOException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+		Properties p = new Properties();
+		p.load(ClassLoader.getSystemResourceAsStream("mapping.properties"));
+		for(Object key : p.keySet()){
+			String className = p.getProperty((String)key);
+			@SuppressWarnings("unchecked")
+			Class<IHandler> o = (Class<IHandler>)Class.forName(className);
+			HandlerManager.regHandler((String)key, o.newInstance());
 		}
 	}
-
-	public static void initHandlers() throws HandlerExistedException {
-		HandlerManager.regHandler("/api/deviceGrouping/upload", new GroupUploadHandler());
-		HandlerManager.regHandler("/api/deviceGrouping/info", new GroupInfoHandler());
-		HandlerManager.regHandler("/api/softwareUpgrading/upgradeUpload", new UpgradeUploadHandler());
-		HandlerManager.regHandler("/api/softwareUpgrading/upgradeInfo", new UpgradeInfoHandler());
-		HandlerManager.regHandler("/api/user/register", new UserRegisterHandler());
-		HandlerManager.regHandler("/api/user/login", new UserLoginHandler());
-		HandlerManager.regHandler("/api/user/devices", new UserDevicesHandler());
-		HandlerManager.regHandler("/api/device/register", new DeviceRegisterHandler());
-		HandlerManager.regHandler("/api/device/login", new DeviceLoginHandler());
-		HandlerManager.regHandler("/api/bind/in", new BindInHandler());
-		HandlerManager.regHandler("/api/bind/out", new BindOutHandler());
-		HandlerManager.regHandler("/api/user/changeNick", new UserChangeNickHandler());
-		HandlerManager.regHandler("/api/user/changePass", new UserChangePassHandler());
-	}
-
+	
 	public static void main(String[] args) throws Exception {
 		int port;
 		if (args.length > 0) {
@@ -79,7 +34,7 @@ public class Main {
 			port = 8080;
 		}
 		logger.info("Start Port {}", port);
-		initHandlers();
+		bindHandler();
 		new App(port).run();
 	}
 }
