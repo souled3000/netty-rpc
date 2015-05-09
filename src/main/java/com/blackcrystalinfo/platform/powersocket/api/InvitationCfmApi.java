a8a6377abd9000169a00723cb23e9b9c74ff8eb0
lchj <licj@blackcrystalinfo.com>
2015-05-09 11:38:54 +0800

    adding json msg about info of adding mems for InvitationCfm.java

--- src/main/java/com/blackcrystalinfo/platform/powersocket/api/InvitationCfmApi.java
+++ src/main/java/com/blackcrystalinfo/platform/powersocket/api/InvitationCfmApi.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 
 import redis.clients.jedis.Jedis;
 
+import com.alibaba.fastjson.JSON;
 import com.blackcrystalinfo.platform.HandlerAdapter;
 import com.blackcrystalinfo.platform.RpcRequest;
 import com.blackcrystalinfo.platform.annotation.Path;
@@ -21,59 +22,66 @@ import com.blackcrystalinfo.platform.util.CookieUtil;
 import com.blackcrystalinfo.platform.util.DataHelper;
 import com.blackcrystalinfo.platform.util.Utils;
 
-@Path(path="/mobile/invitationcfm")
+@Path(path = "/mobile/invitationcfm")
 public class InvitationCfmApi extends HandlerAdapter {
 	private static final Logger logger = LoggerFactory.getLogger(InvitationCfmApi.class);
+
 	public Object rpc(RpcRequest req) throws Exception {
-		Map<Object,Object> r = new HashMap<Object,Object>();
+		Map<Object, Object> r = new HashMap<Object, Object>();
 		r.put(status, SYSERROR.toString());
-		
+
 		String uId = CookieUtil.gotUserIdFromCookie(req.getParameter("cookie"));
 		String oper = req.getParameter("uId");
 		String asw = req.getParameter("asw");
 		Jedis j = null;
-		try{
-			j= DataHelper.getJedis();
-			
-			if("yes".equals(asw)){
-				
-				String operFamily = j.hget("user:family", oper);
-				
-				if(StringUtils.isBlank(operFamily)){
+		try {
+			j = DataHelper.getJedis();
+
+			String operFamily = j.hget("user:family", oper);
+			String mnick = j.hget("user:nick", operFamily);
+			String nick = j.hget("user:nick", uId);
+			StringBuilder msg = new StringBuilder();
+			Map<String, String> mm = new HashMap<String, String>();
+			mm.put("hostId", oper);
+			mm.put("hostNick", mnick);
+			mm.put("mId", uId);
+			mm.put("mNick", nick);
+			msg.append(JSON.toJSON(mm));
+
+			if ("yes".equals(asw)) {
+				if (StringUtils.isBlank(operFamily)) {
 					j.hset("user:family", oper, oper);
+					j.sadd("family:" + oper, oper);
 				}
 				j.hset("user:family", uId, oper);
-				j.sadd("family:"+oper, uId);
-				j.sadd("family:"+oper, oper);
-				
-				
-				
-				//获取家庭所有设备
-				Set<String>members = j.smembers("family:"+oper);
-				for(String m : members){
-					Set<String> devices = j.smembers("u:"+m+":devices");
-					for(String o : devices){
+				j.sadd("family:" + oper, uId);
+
+				// 获取家庭所有设备
+				Set<String> members = j.smembers("family:" + oper);
+				for (String m : members) {
+					Set<String> devices = j.smembers("u:" + m + ":devices");
+					for (String o : devices) {
 						StringBuilder sb = new StringBuilder();
-						//将uid与oper下的所有设备做关联
+						// 将uid与oper下的所有设备做关联
 						sb.append(o).append("|").append(uId).append("|").append("1");
 						j.publish("PubDeviceUsers", sb.toString());
 					}
 				}
-				String memlist = StringUtils.join(members.iterator(), ",")+"|";
-				
-				j.publish("PubCommonMsg:0x36".getBytes(), Utils.genMsg(memlist, 1, Long.parseLong(uId), ""));
-			}else{
-				j.publish("PubCommonMsg:0x36".getBytes(), Utils.genMsg(String.valueOf(oper)+"|", 1, Long.parseLong(uId), ""));
-				
+				String memlist = StringUtils.join(members.iterator(), ",") + "|";
+
+				j.publish("PubCommonMsg:0x36".getBytes(), Utils.genMsg(memlist, 1/** 用户同意加入家庭 */ , Long.parseLong(uId), msg.toString()));
+			} else {
+				j.publish("PubCommonMsg:0x36".getBytes(), Utils.genMsg(String.valueOf(oper) + "|", 6/** 用户拒绝加入家庭 */ , Long.parseLong(uId), msg.toString()));
+
 			}
-		}catch(Exception e){
-			logger.error("",e);
+		} catch (Exception e) {
+			logger.error("", e);
 			DataHelper.returnBrokenJedis(j);
-		}finally{
+		} finally {
 			DataHelper.returnJedis(j);
 		}
-		
-		r.put(status,SUCCESS.toString());
+
+		r.put(status, SUCCESS.toString());
 		return r;
 	}
 }
